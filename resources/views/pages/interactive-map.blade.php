0b0d2556ade1f52d1146e374ecf6747e638567f1
Eneki <joanreneki@gmail.com>
2019-10-08 22:31:10 +0300

    remove banner

--- resources/views/pages/interactive-map.blade.php
+++ resources/views/pages/interactive-map.blade.php
@@ -1,30 +1,5 @@
 @extends('layouts.layout')
 
-@section('content')
-    {{--Banner--}}
-    <section class="section section-alt">
-        <div class="row-fluid">
-            <div class="super-hero-unit">
-                <figure class="dark-background">
-                    <img alt="river meandering through a shallow valley"
-                         src="{{ asset('/img/assets/feow/ground-1.jpeg') }}" class="half-opacity">
-                    <figcaption class="flex-caption">
-                        <h1 class="super animated fadeinup delayedmore">
-                            Interactive Map
-                        </h1>
-                    </figcaption>
-                </figure>
-            </div>
-        </div>
-    </section>
-    {{--Main Content--}}
-    <section class="section section-padded">
-        <div class="container-fluid">
-            <div id="viewDiv"></div>
-        </div>
-    </section>
-@stop
-
 @section('scripts')
     <script src="https://js.arcgis.com/4.12/"></script>
     <script>
