c8d1bbe110d373ff9a2bff756e7a259f1bc0d38c
Nasser Olwero <jgnasser@gmail.com>
2019-10-17 07:24:52 -0400

    Moved download files

--- resources/views/pages/downloads.blade.php
+++ resources/views/pages/downloads.blade.php
@@ -21,7 +21,7 @@
     <section class="section custom-section-padded">
         <div class="body-padding body-text">
             @foreach ($downloads as $download)
-                <p><strong>{{ $download->title }}<a href="{{ asset('/img/assets/feow/'.$download->link) }}" download><i class="fas fa-cloud-download-alt"></i></a></strong></p>
+                <p><strong>{{ $download->title }}<a href="{{ asset('/files/downloads/'.$download->link) }}" download><i class="fas fa-cloud-download-alt"></i></a></strong></p>
                 <p>{!! $download->description !!}</p>
                 <br>
             @endforeach
